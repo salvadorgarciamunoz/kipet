c2fe3ada4a603c085c75498fab63287a8511c471
Michael Short <mchlshort@gmail.com>
2018-05-21 17:20:02 -0400

    changing init

--- kipet/library/__init__.py
+++ kipet/library/__init__.py
@@ -8,17 +8,17 @@ try:
         import imp 
         imp.find_module('casadi') 
     from kipet.library.CasadiModel import CasadiModel 
-    from kipet.library.CasadiModel import KipetCasadiStruct 
+    from kipet.library.CasadiModel import KipetCasadiStruct
     found_casadi = True 
 except ImportError: 
     found_casadi = False 
  
  
 if found_casadi: 
-    __all__ = ['CasadiModel.py','TemplateBuilder','BaseAbstractModel','CasadiSimulator',
+    __all__ = ['CasadiModel','TemplateBuilder','BaseAbstractModel','CasadiSimulator',
                'data_tools','fe_factory','Optimizer','ParameterEstimator','PyomoSimulator',
                'ResultsObject','Simulator','VarianceEstimator'] 
 else: 
-    __all__ = ['TemplateBuilder','BaseAbstractModel','CasadiSimulator',
+    __all__ = ['TemplateBuilder','BaseAbstractModel',
                'data_tools','fe_factory','Optimizer','ParameterEstimator',
                'PyomoSimulator','ResultsObject','Simulator','VarianceEstimator']  
