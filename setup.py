a4254382a67fc05eff33a8d5e22be7f27b854eb9
Michael Short <mchlshort@gmail.com>
2019-01-03 11:08:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 from distutils.core import Extension
 
 DISTNAME = 'kipet'
-VERSION = '1.0.0'
+VERSION = '1.0.9'
 PACKAGES = find_packages()
 EXTENSIONS = []
 DESCRIPTION = 'Package for kinetic estimation based on spectral or concentration data'
 LONG_DESCRIPTION = '' #open('README.md').read()
 AUTHOR = 'Jose-Santiago-Rodriguez, Salvador Garcia-Munoz, Lorentz T. Biegler, David M. Thierry, Christina Schenk, Michael Short '
-MAINTAINER_EMAIL = 'TODO'
+MAINTAINER_EMAIL = 'shortm@andrew.cmu.edu'
 LICENSE = 'GPL-3'
-URL = 'https://github.com/salvadorgarciamunoz/KIPET'
+URL = 'https://github.com/salvadorgarciamunoz/kipet'
 
 setuptools_kwargs = {
     'zip_safe': False,
