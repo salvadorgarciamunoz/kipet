8becf424466251df065a66ecd19f4e0bab9e266c
Michael Short <mchlshort@gmail.com>
2019-06-25 11:01:38 -0400

    New variance estimator examples

--- kipet/examples/Ex_13_alt_variance_tutorial.py
+++ kipet/examples/Ex_13_alt_variance_tutorial.py
@@ -1,12 +1,10 @@
-
 #  _________________________________________________________________________
 #
 #  Kipet: Kinetic parameter estimation toolkit
-#  Copyright (c) 2016 Eli Lilly.
 #  _________________________________________________________________________
 
 # Sample Problem 
-# Estimation with unknow variancesof spectral data using pyomo discretization 
+# Estimation with unknow variances of spectral data using pyomo discretization 
 #
 #		\frac{dZ_a}{dt} = -k_1*Z_a	                Z_a(0) = 1
 #		\frac{dZ_b}{dt} = k_1*Z_a - k_2*Z_b		Z_b(0) = 0
@@ -33,30 +31,28 @@ if __name__ == "__main__":
     if len(sys.argv)==2:
         if int(sys.argv[1]):
             with_plots = False
- 
         
     #=========================================================================
     #USER INPUT SECTION - REQUIRED MODEL BUILDING ACTIONS
-    #=========================================================================
-       
+    #=========================================================================       
     
     # Load spectral data from the relevant file location. As described in section 4.3.1
     #################################################################################
     dataDirectory = os.path.abspath(
         os.path.join( os.path.dirname( os.path.abspath( inspect.getfile(
             inspect.currentframe() ) ) ), 'data_sets'))
-    filename =  os.path.join(dataDirectory,'Dij.txt')
-    D_frame = read_spectral_data_from_txt(filename)
+    filename =  os.path.join(dataDirectory,'varest2.csv')
+    D_frame = read_spectral_data_from_csv(filename, negatives_to_zero = True)
 
     # Then we build dae block for as described in the section 4.2.1. Note the addition
     # of the data using .add_spectral_data
     #################################################################################    
     builder = TemplateBuilder()    
-    components = {'A':1e-3,'B':0,'C':0}
+    components = {'A':1e-2,'B':0,'C':0}
     builder.add_mixture_component(components)
-    builder.add_parameter('k1', init=2.0, bounds=(0.05,5.0)) 
+    builder.add_parameter('k1', init=1.2, bounds=(0.01,5.0)) 
     #There is also the option of providing initial values: Just add init=... as additional argument as above.
-    builder.add_parameter('k2',init = 0.2, bounds=(0.0,1.0))
+    builder.add_parameter('k2',init = 0.2, bounds=(0.001,1.0))
     builder.add_spectral_data(D_frame)
 
     # define explicit system of ODEs
@@ -75,9 +71,10 @@ if __name__ == "__main__":
     #=========================================================================
     # For this problem we have an input D matrix that has some noise in it
     # We can therefore use the variance estimator described in the Overview section
-    # of the documentation and Section 4.3.3
+    # of the documentation and Section 4.3.3 or we can use the alternative methodology,
+    # whereby we directly solve for the sigmas given different values of device variance
     v_estimator = VarianceEstimator(opt_model)
-    v_estimator.apply_discretization('dae.collocation',nfe=100,ncp=3,scheme='LAGRANGE-RADAU')
+    v_estimator.apply_discretization('dae.collocation',nfe=50,ncp=3,scheme='LAGRANGE-RADAU')
     
     # It is often requried for larger problems to give the solver some direct instructions
     # These must be given in the form of a dictionary
@@ -87,37 +84,53 @@ if __name__ == "__main__":
     # options['bound_push'] = 1e-8
     # options['tol'] = 1e-9
     options['linear_solver'] = 'ma57'
-    options['max_iter'] = 2000
     
     # The set A_set is then decided. This set, explained in Section 4.3.3 is used to make the
     # variance estimation run faster and has been shown to not decrease the accuracy of the variance 
     # prediction for large noisey data sets.
-    A_set = [l for i,l in enumerate(opt_model.meas_lambdas) if (i % 5 == 0)]
+    #A_set = [l for i,l in enumerate(opt_model.meas_lambdas) if (i % 4 == 0)]
+    # For this small problem we do not need this, however it should be set for larger problems
+    
+    # If we do not wish to use the Chen et al. way of solving for variances, there is a simpler
+    # and often more reliable way of finding variances. This is done by first assuming we have no model 
+    # variance and solving for the device variance. See details in the documentation under Section XXX
+    # This value will give us our worst possible device variance (i.e. all variance is explained using 
+    # the device variance)
     
     worst_case_device_var = v_estimator.solve_max_device_variance('ipopt', 
                                                                   tee = False, 
-                                                                  subset_lambdas = A_set,
+                                                                  #subset_lambdas = A,
                                                                   solver_opts = options)
     
-    best_possible_accuracy = 1e-7
+    # Now that we have the worst case device variance we wish to solve the maximum likelihood
+    # problem with different values of delta based on this information and device information 
+    # for best and worst-case performance. We will solve for different values of model variance,
+    # known as sigma, at each value. We can provide a range, based on the value from above and
+    # device specifications and we can set a number of points to search within that range.
+    best_possible_accuracy = 1e-8
     search_range = (best_possible_accuracy, worst_case_device_var)
-    num_points = 20
+    num_points = 10
     # This will provide a list of sigma values based on the different delta values evaluated.
     results_variances = v_estimator.run_opt('ipopt',
                                             method = 'direct_sigmas',
                                             tee=False,
                                             solver_opts=options,
                                             num_points = num_points,                                            
-                                            subset_lambdas=A_set,
+                                            #subset_lambdas=A_set,
                                             device_range = search_range)
     
-    delta = 1e-6
+    # It is suggested that from this pool of solutions for different delta and sigma values, that the 
+    # best choice for sigma and delta will then be which provided the parameter values closest to 
+    # the initial guesses.
+
+    # it is also possible to solve directly for sigmas based on a specific value for delta using
+    delta = 1e-7
     results_vest = v_estimator.solve_sigma_given_delta('ipopt', 
-                                                         subset_lambdas= A_set, 
+                                                         #subset_lambdas= A, 
                                                          solver_opts = options, 
                                                          tee=False,
                                                          delta = delta)
-   
+    
     # Variances can then be displayed 
     print("\nThe estimated variances are:\n")
     for k,v in six.iteritems(results_vest):
@@ -127,6 +140,7 @@ if __name__ == "__main__":
    # sigmas = sigmas
     
     sigmas = results_vest
+
     #=========================================================================
     # USER INPUT SECTION - PARAMETER ESTIMATION 
     #=========================================================================
@@ -135,7 +149,7 @@ if __name__ == "__main__":
 
     # and define our parameter estimation problem and discretization strategy
     p_estimator = ParameterEstimator(opt_model)
-    p_estimator.apply_discretization('dae.collocation',nfe=100,ncp=3,scheme='LAGRANGE-RADAU')
+    p_estimator.apply_discretization('dae.collocation',nfe=60,ncp=1,scheme='LAGRANGE-RADAU')
     
     # Certain problems may require initializations and scaling and these can be provided from the 
     # varininace estimation step. This is optional.
