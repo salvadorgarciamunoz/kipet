3e24437fb28a8006fca746aa1e0ca227ecf78f78
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2020-10-14 09:16:17 -0400

    updated example 6

--- kipet/new_examples/Ex_6_non_absorbing.py
+++ kipet/new_examples/Ex_6_non_absorbing.py
@@ -17,18 +17,20 @@ if __name__ == "__main__":
        
     kipet_model = KipetModel()
     
+    r1 = kipet_model.new_reaction('reaction-1')
+    
     # Add the model parameters
-    kipet_model.add_parameter('k1', init=2, bounds=(0.1, 5.0))
-    kipet_model.add_parameter('k2', init=0.2, bounds=(0.01, 2.0))
+    r1.add_parameter('k1', init=2, bounds=(0.1, 5.0))
+    r1.add_parameter('k2', init=0.2, bounds=(0.01, 2.0))
     
     # Declare the components and give the initial values
-    kipet_model.add_component('A', state='concentration', init=1e-3)
-    kipet_model.add_component('B', state='concentration', init=0.0)
-    kipet_model.add_component('C', state='concentration', init=0.0)
+    r1.add_component('A', state='concentration', init=1e-3)
+    r1.add_component('B', state='concentration', init=0.0)
+    r1.add_component('C', state='concentration', init=0.0)
     
     # Use this function to replace the old filename set-up
-    filename = kipet_model.set_directory('Dij.txt')
-    kipet_model.add_dataset('D_frame', category='spectral', file=filename)
+    filename = r1.set_directory('Dij.txt')
+    r1.add_dataset('D_frame', category='spectral', file=filename)
 
     # define explicit system of ODEs
     def rule_odes(m,t):
@@ -38,31 +40,25 @@ if __name__ == "__main__":
         exprs['C'] = m.P['k2']*m.Z[t,'B']
         return exprs
     
-    kipet_model.add_equations(rule_odes)
+    r1.add_equations(rule_odes)
     
     # If no times are given to the builder, it will use the times in the data
-    kipet_model.set_non_absorbing_species(['C'])
-    
-    # Display the KipetTemplate object attributes
-    print(kipet_model)
+    r1.set_non_absorbing_species(['C'])
 
     # Settings
-    kipet_model.settings.collocation.ncp = 1
-    kipet_model.settings.collocation.nfe = 60
-    kipet_model.settings.variance_estimator.use_subset_lambdas = True
-    kipet_model.settings.variance_estimator.max_iter = 5
-    kipet_model.settings.variance_estimator.tolerance = 1e-4
-    kipet_model.settings.parameter_estimator.tee = False
-    
-    # Show the KipetModel settings
-    print(kipet_model.settings)
+    r1.settings.collocation.ncp = 1
+    r1.settings.collocation.nfe = 60
+    r1.settings.variance_estimator.use_subset_lambdas = True
+    r1.settings.variance_estimator.max_iter = 5
+    r1.settings.variance_estimator.tolerance = 1e-4
+    r1.settings.parameter_estimator.tee = False
     
     # This is all you need to run KIPET!
-    kipet_model.run_opt()
+    r1.run_opt()
     
     # Display the results
-    kipet_model.results.show_parameters
+    r1.results.show_parameters
     
        # New plotting methods
     if with_plots:
-        kipet_model.results.plot()
+        r1.results.plot()
