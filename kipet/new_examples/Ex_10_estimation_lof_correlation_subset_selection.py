015574f563b0df7708e5a0dda72e4e24aaa5b1b1
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2020-10-14 13:55:28 -0400

    updated example 10

--- kipet/new_examples/Ex_10_estimation_lof_correlation_subset_selection.py
+++ kipet/new_examples/Ex_10_estimation_lof_correlation_subset_selection.py
@@ -18,18 +18,20 @@ if __name__ == "__main__":
  
     kipet_model = KipetModel()
     
+    r1 = kipet_model.new_reaction('reaction-1')
+    
     # Add the model parameters
-    kipet_model.add_parameter('k1', init=4.0, bounds=(0.0, 5.0))
-    kipet_model.add_parameter('k2', init=0.5, bounds=(0.0, 1.0))
+    r1.add_parameter('k1', init=4.0, bounds=(0.0, 5.0))
+    r1.add_parameter('k2', init=0.5, bounds=(0.0, 1.0))
     
     # Declare the components and give the initial values
-    kipet_model.add_component('A', state='concentration', init=1e-3)
-    kipet_model.add_component('B', state='concentration', init=0.0)
-    kipet_model.add_component('C', state='concentration', init=0.0)
+    r1.add_component('A', state='concentration', init=1e-3)
+    r1.add_component('B', state='concentration', init=0.0)
+    r1.add_component('C', state='concentration', init=0.0)
     
     # Use this function to replace the old filename set-up
-    filename = kipet_model.set_directory('Dij.txt')
-    kipet_model.add_dataset('D_frame', category='spectral', file=filename)
+    filename = r1.set_directory('Dij.txt')
+    r1.add_dataset('D_frame', category='spectral', file=filename)
 
     # define explicit system of ODEs
     def rule_odes(m,t):
@@ -39,49 +41,49 @@ if __name__ == "__main__":
         exprs['C'] = m.P['k2']*m.Z[t,'B']
         return exprs
     
-    kipet_model.add_equations(rule_odes)
+    r1.add_equations(rule_odes)
     
-    #kipet_model.bound_profile(var='S', bounds=(0, 200))
+    #r1.bound_profile(var='S', bounds=(0, 200))
 
     # Display the KipetTemplate object attributes
-    print(kipet_model)
+    print(r1)
 
     # Settings
-    kipet_model.settings.collocation.ncp = 3
-    kipet_model.settings.collocation.nfe = 60
-    kipet_model.settings.variance_estimator.use_subset_lambdas = True
-    kipet_model.settings.variance_estimator.tolerance = 1e-5
-    kipet_model.settings.parameter_estimator.tee = False
+    r1.settings.collocation.ncp = 3
+    r1.settings.collocation.nfe = 60
+    r1.settings.variance_estimator.use_subset_lambdas = True
+    r1.settings.variance_estimator.tolerance = 1e-5
+    r1.settings.parameter_estimator.tee = False
     
     # Show the KipetModel settings
-    print(kipet_model.settings)
+    print(r1.settings)
     
     # This is all you need to run KIPET!
-    #kipet_model.set_times(0, 10)
-    kipet_model.run_opt()
+    #r1.set_times(0, 10)
+    r1.run_opt()
     
     # Display the results
-    kipet_model.results.show_parameters
+    r1.results.show_parameters
         
-    kipet_model.results.plot(show_plot=with_plots)
+    r1.results.plot(show_plot=with_plots)
     
    
     """Wavelength subset selection methods"""
     
     # See the tutorial for more info: Tutorial 13
-    lof = kipet_model.lack_of_fit()
-    correlations = kipet_model.wavelength_correlation()
-    #kipet_model.run_lof_analysis()
+    lof = r1.lack_of_fit()
+    correlations = r1.wavelength_correlation()
+    #r1.run_lof_analysis()
     
     # Follow the tutorial for this next step:
     # A specific range is selected and smaller steps are made
-    #kipet_model.run_lof_analysis(step_size = 0.01, search_range = (0, 0.12))
+    #r1.run_lof_analysis(step_size = 0.01, search_range = (0, 0.12))
     
     # It seems like this is a good cut-off point
-    subset = kipet_model.wavelength_subset_selection(n=0.095) 
+    subset = r1.wavelength_subset_selection(n=0.095) 
     
     # Solve the ParameterEstimator using the wavelength subset
-    subset_results = kipet_model.run_opt_with_subset_lambdas(subset)
+    subset_results = r1.run_opt_with_subset_lambdas(subset) 
     
     # Display the new results
     subset_results.show_parameters
