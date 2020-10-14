e27ed1f59d02c58f2dbb484cc9abbdaf71250a5f
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2020-10-14 14:00:03 -0400

    updated example 12

--- kipet/new_examples/Ex_12_multiple_experiments_concentration.py
+++ kipet/new_examples/Ex_12_multiple_experiments_concentration.py
@@ -1,5 +1,7 @@
-"""Example 11: Multiple Experimental Datasets with the new KipetModel
+"""Example 12: Multiple Experimental Datasets with the new KipetModel
  
+This examples uses two reactions with concentration data where the second data
+set is noisy
 """
 # Standard library imports
 import sys # Only needed for running the example from the command line
@@ -7,7 +9,7 @@ import sys # Only needed for running the example from the command line
 # Third party imports
 
 # Kipet library imports
-from kipet.kipet import KipetModel, KipetModelBlock
+from kipet.kipet import KipetModel
 
 if __name__ == "__main__":
 
@@ -19,14 +21,16 @@ if __name__ == "__main__":
     # Define the general model
     kipet_model = KipetModel()
     
+    r1 = kipet_model.new_reaction(name='reaction-1')
+    
     # Add the model parameters
-    kipet_model.add_parameter('k1', init=1.0, bounds=(0.0, 10.0))
-    kipet_model.add_parameter('k2', init=0.224, bounds=(0.0, 10.0))
+    r1.add_parameter('k1', init=1.0, bounds=(0.0, 10.0))
+    r1.add_parameter('k2', init=0.224, bounds=(0.0, 10.0))
     
     # Declare the components and give the initial values
-    kipet_model.add_component('A', state='concentration', init=1e-3)
-    kipet_model.add_component('B', state='concentration', init=0.0)
-    kipet_model.add_component('C', state='concentration', init=0.0)
+    r1.add_component('A', state='concentration', init=1e-3)
+    r1.add_component('B', state='concentration', init=0.0)
+    r1.add_component('C', state='concentration', init=0.0)
     
     # define explicit system of ODEs
     def rule_odes(m,t):
@@ -36,48 +40,34 @@ if __name__ == "__main__":
         exprs['C'] = m.P['k2']*m.Z[t,'B']
         return exprs
     
-    kipet_model.add_equations(rule_odes)
-    
-    # For clarity, the first model is cloned although this is not necessary
-    kipet_model1 = kipet_model.clone(name='Model-1')
-  
+    r1.add_equations(rule_odes)
+   
     # Add the dataset for the first model
-    filename1 = kipet_model1.set_directory('Ex_1_C_data.txt')
-    kipet_model1.add_dataset('C_frame1', category='concentration', file=filename1)
+    r1.add_dataset(file='Ex_1_C_data.txt')
+    
+    # Add the known variances
+    r1.variances = {'A':1e-10,'B':1e-10,'C':1e-10}
 
     # Repeat for the second model - the only difference is the dataset    
-    kipet_model2 = kipet_model.clone(name='Model-2')
-
+    r2 = kipet_model.new_reaction(name='reaction_2', model_to_clone=r1, items_not_copied='datasets')
+    r2.variances = {'A':1e-10,'B':1e-10,'C':1e-10}
     # Simulated second dataset with noise
-    noised_data = kipet_model1.add_noise_to_data(kipet_model1.datasets['C_frame1'].data, 0.0001) 
+    noised_data = kipet_model.add_noise_to_data(r1.datasets['C_data'].data, 0.0001) 
     
     # Add the dataset for the second model
-    kipet_model2.add_dataset('C_frame2', category='concentration', data=noised_data)
+    r2.add_dataset(data=noised_data[::10])
 
-    # Create the KipetModelBlock instance to hold the KipetModels
-    model_block = KipetModelBlock()
-    model_block.add_model(kipet_model1)
-    model_block.add_model(kipet_model2)
-    
-    """Using confidence intervals - uncomment the following two lines"""
-    # If using knonw variances, add them to the create method
-    # This will skip the variance estimation step
-    
-    user_provided_variances = {'A':1e-10,'B':1e-10,'C':1e-10}
+    # Concentration data requires variances as input
+    #user_provided_variances = {'A':1e-10,'B':1e-10,'C':1e-10}
+    #user_provided_variances = {}    
     
     # If you want the confidence intervals, change the default solver
-    model_block.settings.parameter_estimator.solver = 'ipopt_sens'
-    model_block.settings.parameter_estimator.covariance = True
-    
-    #model_block.settings.parameter_estimator.spectra_problem = False
-    
-    """End of confidence interval section"""
+    kipet_model.settings.parameter_estimator.solver = 'ipopt_sens'
     
     # Create the MultipleExperimentsEstimator and perform the parameter fitting
-    model_block.create_multiple_experiments_estimator(variances=user_provided_variances)
-    model_block.run_multiple_experiments_estimator()
+    kipet_model.run_opt()
 
     # Plot the results
-    for model, results in model_block.results.items():
+    for model, results in kipet_model.results.items():
         results.show_parameters
         results.plot(show_plot=with_plots)
