7d6886dbcbb23e1790e2ad924efd54f410040276
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2020-10-14 08:50:46 -0400

    Updated example 2 no model noise

--- kipet/new_examples/Ex_2_estimation_no_model_noise.py
+++ kipet/new_examples/Ex_2_estimation_no_model_noise.py
@@ -19,18 +19,20 @@ if __name__ == "__main__":
  
     kipet_model = KipetModel()
     
+    r1 = kipet_model.new_reaction('reaction-1')
+    
     # Add the model parameters
-    kipet_model.add_parameter('k1', init=2, bounds=(0.0, 5.0))
-    kipet_model.add_parameter('k2', init=0.2, bounds=(0.0, 2.0))
+    r1.add_parameter('k1', init=2, bounds=(0.0, 5.0))
+    r1.add_parameter('k2', init=0.2, bounds=(0.0, 2.0))
     
     # Declare the components and give the initial values
-    kipet_model.add_component('A', state='concentration', init=1e-3)
-    kipet_model.add_component('B', state='concentration', init=0.0)
-    kipet_model.add_component('C', state='concentration', init=0.0)
+    r1.add_component('A', state='concentration', init=1e-3)
+    r1.add_component('B', state='concentration', init=0.0)
+    r1.add_component('C', state='concentration', init=0.0)
     
     # Use this function to replace the old filename set-up
-    filename = kipet_model.set_directory('Dij.txt')
-    kipet_model.add_dataset('D_frame', category='spectral', file=filename)
+    filename = r1.set_directory('Dij.txt')
+    r1.add_dataset('D_frame', category='spectral', file=filename)
 
     # define explicit system of ODEs
     def rule_odes(m,t):
@@ -40,33 +42,33 @@ if __name__ == "__main__":
         exprs['C'] = m.P['k2']*m.Z[t,'B']
         return exprs
     
-    kipet_model.add_equations(rule_odes)
+    r1.add_equations(rule_odes)
     
-    kipet_model.bound_profile(var='S', bounds=(0, 200))
+    r1.bound_profile(var='S', bounds=(0, 200))
     
     # Display the KipetTemplate object attributes
-    print(kipet_model)
+    print(r1)
 
     # Settings
-    kipet_model.settings.general.initialize_pe = False
-    kipet_model.settings.general.scale_pe = False
-    kipet_model.settings.collocation.ncp = 1
-    kipet_model.settings.collocation.nfe = 100
-    kipet_model.settings.variance_estimator.use_subset_lambdas = False
-    kipet_model.settings.variance_estimator.max_device_variance = True
-    kipet_model.settings.variance_estimator.tee = False
+    r1.settings.general.initialize_pe = False
+    r1.settings.general.scale_pe = False
+    r1.settings.collocation.ncp = 1
+    r1.settings.collocation.nfe = 100
+    r1.settings.variance_estimator.use_subset_lambdas = False
+    r1.settings.variance_estimator.max_device_variance = True
+    r1.settings.variance_estimator.tee = False
     
-    kipet_model.settings.parameter_estimator.solver = 'k_aug'
+    r1.settings.parameter_estimator.solver = 'k_aug'
     
     # Show the KipetModel settings
-    print(kipet_model.settings)
+    print(r1.settings)
     
     # This is all you need to run KIPET!
-    kipet_model.run_opt()
+    r1.run_opt()
     
     # Display the results
-    kipet_model.results.show_parameters
+    r1.results.show_parameters
     
        # New plotting methods
     if with_plots:
-        kipet_model.results.plot('Z', show_exp=False)
+        r1.results.plot('Z', show_exp=False)
