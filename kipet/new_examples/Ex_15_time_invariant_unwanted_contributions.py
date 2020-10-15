efe0c40fe31c0bbcb1d8c5f4647249f66f8e556b
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2020-10-15 07:45:47 -0400

    updated example 15, single reactions

--- kipet/new_examples/Ex_15_time_invariant_unwanted_contributions.py
+++ kipet/new_examples/Ex_15_time_invariant_unwanted_contributions.py
@@ -21,18 +21,19 @@ if __name__ == "__main__":
     # Define the general model
     kipet_model = KipetModel()
     
-    # Add the data
-    filename = kipet_model.set_directory('Dij_tv_G.txt')
-    kipet_model.add_dataset('D_frame', category='spectral', file=filename)
+    r1 = kipet_model.new_reaction('reaction-1')
     
     # Add the model parameters
-    kipet_model.add_parameter('k1', init=1.4, bounds=(0.0, 2.0))
-    kipet_model.add_parameter('k2', init=0.25, bounds=(0.0, 0.5))
+    r1.add_parameter('k1', init=1.4, bounds=(0.0, 2.0))
+    r1.add_parameter('k2', init=0.25, bounds=(0.0, 0.5))
     
     # Declare the components and give the initial values
-    kipet_model.add_component('A', state='concentration', init=1e-2)
-    kipet_model.add_component('B', state='concentration', init=0.0)
-    kipet_model.add_component('C', state='concentration', init=0.0)
+    r1.add_component('A', state='concentration', init=1e-2)
+    r1.add_component('B', state='concentration', init=0.0)
+    r1.add_component('C', state='concentration', init=0.0)
+    
+    # Add the data
+    r1.add_dataset(category='spectral', file='Dij_tv_G.txt')
     
     # define explicit system of ODEs
     def rule_odes(m,t):
@@ -42,13 +43,13 @@ if __name__ == "__main__":
         exprs['C'] = m.P['k2']*m.Z[t,'B']
         return exprs
     
-    kipet_model.add_equations(rule_odes)
+    r1.add_equations(rule_odes)
     
     # Settings
-    #kipet_model.settings.general.initialize_pe = False
-    #kipet_model.settings.general.no_user_scaling = True
+    #r1.settings.general.initialize_pe = False
+    #r1.settings.general.no_user_scaling = True
     
-    kipet_model.settings.collocation.nfe = 100
+    r1.settings.collocation.nfe = 100
     
     # For the "time-invariant" unwanted contribuiton cases, stoichiometric coefficients
     # of each reaction (St) and/or dosing concentration for each dosing time (Z_in) are required
@@ -66,20 +67,20 @@ if __name__ == "__main__":
     # Assuming we know the "time_invariant" unwanted contribution is included,
     # so set the option time_invariant_G=True. St and Z_in are transmitted as well.
     # finally we run the optimization
-    kipet_model.settings.parameter_estimator.G_contribution = 'time_invariant_G'
-    kipet_model.settings.parameter_estimator.St = St    
-    # kipet_model.settings.parameter_estimator.Z_in = Z_in
+    r1.settings.parameter_estimator.G_contribution = 'time_invariant_G'
+    r1.settings.parameter_estimator.St = St    
+    # r1.settings.parameter_estimator.Z_in = Z_in
 
     # Run KIPET
-    kipet_model.run_opt()
-    kipet_model.results.show_parameters
+    r1.run_opt()
+    r1.results.show_parameters
 
-    kipet_model.results.plot(show_plot=with_plots)
+    r1.results.plot(show_plot=with_plots)
         
     """We can now compare the results with the known profiles"""
     
     # Read the true S to compare with results
-    S_true_filename = kipet_model.set_directory("S_True_for_unwanted_G.csv")
+    S_true_filename = r1.set_directory("S_True_for_unwanted_G.csv")
     S_True = read_file(S_true_filename)
 
     # In this example, we know the magnitude of unwanted contribution.
@@ -99,5 +100,5 @@ if __name__ == "__main__":
         
     # Use the "label" kwarg to add some info to the legend in the plot
     if with_plots:
-        kipet_model.results.plot('S', show_plot=with_plots, extra_data={'data': S_matched, 'label': 'matched'})
+        r1.results.plot('S', show_plot=with_plots, extra_data={'data': S_matched, 'label': 'matched'})
         
--- kipet/new_examples/Ex_15_time_variant_unwanted_contributions.py
+++ kipet/new_examples/Ex_15_time_variant_unwanted_contributions.py
@@ -19,41 +19,41 @@ if __name__ == "__main__":
     # Define the general model
     kipet_model = KipetModel()
     
-    # Add the data
-    filename = kipet_model.set_directory('Dij_tv_G.txt')
-    kipet_model.add_dataset('D_frame', category='spectral', file=filename)
+    r1 = kipet_model.new_reaction('reaction-1')
     
     # Add the model parameters
-    kipet_model.add_parameter('k1', init=1.4, bounds=(0.0, 2.0))
-    kipet_model.add_parameter('k2', init=0.25, bounds=(0.0, 0.5))
+    r1.add_parameter('k1', init=1.4, bounds=(0.0, 2.0))
+    r1.add_parameter('k2', init=0.25, bounds=(0.0, 0.5))
     
     # Declare the components and give the initial values
-    kipet_model.add_component('A', state='concentration', init=1e-2)
-    kipet_model.add_component('B', state='concentration', init=0.0)
-    kipet_model.add_component('C', state='concentration', init=0.0)
+    r1.add_component('A', state='concentration', init=1e-2)
+    r1.add_component('B', state='concentration', init=0.0)
+    r1.add_component('C', state='concentration', init=0.0)
+   
+    r1.add_dataset(category='spectral', file='Dij_tv_G.txt')
     
     # define explicit system of ODEs
     def rule_odes(m,t):
         exprs = dict()
-        exprs['A'] = -m.P['k1']*m.Z[t,'A']
+        exprs['A'] = -m.P['k1']* m.Z[t,'A']
         exprs['B'] = m.P['k1']*m.Z[t,'A']-m.P['k2']*m.Z[t,'B']
         exprs['C'] = m.P['k2']*m.Z[t,'B']
         return exprs
     
-    kipet_model.add_equations(rule_odes)
+    r1.add_equations(rule_odes)
     
     # Settings
-    kipet_model.settings.general.initialize_pe = False
-    kipet_model.settings.general.no_user_scaling = True
+    r1.settings.general.initialize_pe = False
+    r1.settings.general.no_user_scaling = True
     
-    kipet_model.settings.collocation.nfe = 100
-    kipet_model.settings.parameter_estimator.G_contribution = 'time_variant_G'
+    r1.settings.collocation.nfe = 100
+    r1.settings.parameter_estimator.G_contribution = 'time_variant_G'
 
     # Unwanted contribution handling
-    kipet_model.builder.add_qr_bounds_init(bounds=(0,None),init=1.1)
-    kipet_model.builder.add_g_bounds_init(bounds=(0,None))
+    r1.builder.add_qr_bounds_init(bounds=(0,None),init=1.1)
+    r1.builder.add_g_bounds_init(bounds=(0,None))
 
     # Run KIPET
-    kipet_model.run_opt()
+    r1.run_opt()
 
-    kipet_model.results.plot(show_plot=with_plots)
+    r1.results.plot(show_plot=with_plots)
