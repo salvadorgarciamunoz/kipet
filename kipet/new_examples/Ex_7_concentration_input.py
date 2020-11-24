a0ccf4fd06832e8a7a3e72fe0b9666c46613a565
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2020-11-24 04:46:06 -0500

    Added variances back to Ex_7

--- kipet/new_examples/Ex_7_concentration_input.py
+++ kipet/new_examples/Ex_7_concentration_input.py
@@ -24,40 +24,36 @@ if __name__ == "__main__":
     r1.add_parameter('k2', init=0.2, bounds=(0.0, 2.0))
     
     # Declare the components and give the initial values
-    r1.add_component('A', state='concentration', init=0.001)
-    r1.add_component('B', state='concentration', init=0.0)
-    r1.add_component('C', state='concentration', init=0.0)
+    r1.add_component('A', state='concentration', init=0.001, variance=1e-10)
+    r1.add_component('B', state='concentration', init=0.0, variance=1e-11)
+    r1.add_component('C', state='concentration', init=0.0, variance=1e-8)
    
     # Use this function to replace the old filename set-up
     filename = 'example_data/Ex_1_C_data.txt'
     r1.add_dataset('C_data', category='concentration', file=filename)
     
-    # load_data = kipet_model.read_data_file('example_data/test_write.txt')
-    
-    filename_write = 'example_data_new/test_write.txt'
-    kipet_model.write_data_file(filename_write, r1.datasets.datasets['C_data'].data)
-    
     # Define the reaction model
-    # def rule_odes(m,t):
-    #     exprs = dict()
-    #     exprs['A'] = -m.P['k1']*m.Z[t,'A']
-    #     exprs['B'] = m.P['k1']*m.Z[t,'A']-m.P['k2']*m.Z[t,'B']
-    #     exprs['C'] = m.P['k2']*m.Z[t,'B']
-    #     return exprs 
+    def rule_odes(m,t):
+        exprs = dict()
+        exprs['A'] = -m.P['k1']*m.Z[t,'A']
+        exprs['B'] = m.P['k1']*m.Z[t,'A']-m.P['k2']*m.Z[t,'B']
+        exprs['C'] = m.P['k2']*m.Z[t,'B']
+        return exprs 
     
-    # r1.add_equations(rule_odes)
+    r1.add_equations(rule_odes)
     
-    # # Settings
-    # r1.settings.collocation.nfe = 60
+    # Settings
+    r1.settings.collocation.nfe = 60
+    r1.settings.parameter_estimator.solver = 'k_aug'
     
-    # # Run KIPET
-    # r1.run_opt()  
+    # Run KIPET
+    r1.run_opt()  
     
-    # # Display the results
-    # r1.results.show_parameters
+    # Display the results
+    r1.results.show_parameters
 
-    # r1.results.plot('Z', 
-    #                 show_plot=with_plots,
-    #                 description={'title': 'Example 7',
-    #                              'xaxis': 'Time [s]',
-    #                              'yaxis': 'Concentration [mol/L]'})
+    r1.results.plot('Z', 
+                    show_plot=with_plots,
+                    description={'title': 'Example 7',
+                                  'xaxis': 'Time [s]',
+                                  'yaxis': 'Concentration [mol/L]'})
