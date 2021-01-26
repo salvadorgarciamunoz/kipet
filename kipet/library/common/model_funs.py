3fd49f081f578dd2d53a220ac8228c71ca5b1bec
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2021-01-26 09:22:19 -0500

    Added option to reverse the direction of the step

--- kipet/library/common/model_funs.py
+++ kipet/library/common/model_funs.py
@@ -5,7 +5,7 @@ from kipet.library.top_level.variable_names import VariableNames
 
 __var = VariableNames()
 
-def step_fun(model, time_var, time=1e-3, M=1, eta=1e-2, constant=0.5):
+def step_fun(model, time_var, time=1e-3, M=1, eta=1e-2, constant=0.5, on=False):
     """This formulates the step functions for KIPET using a sigmoidal function
     
     Args:
@@ -23,13 +23,15 @@ def step_fun(model, time_var, time=1e-3, M=1, eta=1e-2, constant=0.5):
         step_point (Expression): Returns the pyomo expression for the point
     
     """
+    factor = 1 if not on else -1
+    time *= factor
     pyomo_var = getattr(model, __var.dosing_variable)[time_var, __var.dosing_component]
     
-    step_point = 0.5*((time - pyomo_var) / ((time - pyomo_var)**2 + eta**2/M) ** 0.5) + constant
+    step_point = 0.5*((time - factor*pyomo_var) / ((time - factor*pyomo_var)**2 + eta**2/M) ** 0.5) + constant
     
     return step_point
 
-def step_fun_var_time(model, time_var, M=1, eta=1e-2, constant=0.5):
+def step_fun_var_time(model, time_var, M=1, eta=1e-2, constant=0.5, on=False):
     """This formulates the step functions for KIPET using a sigmoidal function
     
     Args:
@@ -47,10 +49,11 @@ def step_fun_var_time(model, time_var, M=1, eta=1e-2, constant=0.5):
         step_point (Expression): Returns the pyomo expression for the point
     
     """
+    factor = 1 if not on else -1
     #time_var = getattr(model, __var.step_time_var)[__var.step_time_index]
-    time_var_step = model.time_step_change[0] 
+    time_var_step = factor*model.time_step_change[0] 
     pyomo_var = getattr(model, __var.dosing_variable)[time_var, __var.dosing_component]
     
-    step_point = 0.5*((time_var_step - pyomo_var) / ((time_var_step - pyomo_var)**2 + eta**2/M) ** 0.5) + constant
+    step_point = 0.5*((time_var_step - factor*pyomo_var) / ((time_var_step - factor*pyomo_var)**2 + eta**2/M) ** 0.5) + constant
     
     return step_point
