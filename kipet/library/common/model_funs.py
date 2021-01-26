f075612b49ef9abce2c0b37dd1edddfcffe7e677
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2021-01-26 06:53:28 -0500

    cleaned up comments, added step fun for variable time

--- kipet/library/common/model_funs.py
+++ kipet/library/common/model_funs.py
@@ -29,60 +29,27 @@ def step_fun(model, time_var, time=1e-3, M=1, eta=1e-2, constant=0.5):
     
     return step_point
 
-#%%
-
-# def step(t, delta=1e-3, M=1e-8, eps=5e-6, constant=0.5):
+# def step_fun_var_time(model, time=1e-3, M=1, eta=1e-2, constant=0.5):
+#     """This formulates the step functions for KIPET using a sigmoidal function
     
-#     v = 0.5*((delta - t) / ((delta - t)**2 + eps**2/M) ** 0.5) + constant
+#     Args:
+#         pyomo_var: (Var): The algebraic variable used for the step function
+        
+#         delta (float): Time point for step
+        
+#         M (float): Sigmoidal tuning parameter
+        
+#         eta (float): Tuning parameter
+        
+#         constant (float): Constant added to bring to certain value
+        
+#     Returns:
+#         step_point (Expression): Returns the pyomo expression for the point
     
-#     return v
-
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# t = np.linspace(0, 600)
-
-# ts = 210
-
-# plt.figure()
-
-# results = []
-# results2 = []
-# results3 =[]
-
-# for i in t:
+#     """
+#     pyomo_var = getattr(model, __var.dosing_variable)[time_var, __var.dosing_component]
+#     # time_var = getattr(model, __var.step_time_var)[__var.step_time_index]
+     
+#     step_point = 0.5*((time_var - pyomo_var) / ((time_var - pyomo_var)**2 + eta**2/M) ** 0.5) + constant
     
-#     fun1 = step(i, delta=-51, M=1, eps=1e-2, constant=0.5)
-#     fun2 = step(i, delta=210, M=1, eps=1e-2, constant=0.5)
-    
-#     factor = 1#7.27609e-05 
-#     fun = factor*(fun1) # + fun2)
-    
-#     results.append(fun)
-#     results2.append(fun2)
-#     results3.append(fun + fun2)
-    
-# plt.plot(t, results)
-# plt.plot(t, results2)
-# plt.plot(t, results3)
-
-# #%%
-
-# t = np.linspace(-0.0, 600)
-
-# ts = 210
-
-
-# results = []
-# for i in t:
-    
-#     fun1 = step(i, delta=-1, M=1, eps=1e-2, constant=1.5)
-#     # fun2 = step(i, delta=210, M=1, eps=1e-2)
-    
-#     # fun = 7.27609e-05*(fun1 + fun2)
-    
-#     results.append(fun1)
-    
-# plt.plot(t, results)
-
+#     return step_point
