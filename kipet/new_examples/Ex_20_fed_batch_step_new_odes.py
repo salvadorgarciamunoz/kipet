43bec6a378f37cfaee33aaaf327e764663cdccc5
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2021-01-28 11:56:39 -0500

    Cleaned up Ex_20

--- kipet/new_examples/Ex_20_fed_batch_step_new_odes.py
+++ kipet/new_examples/Ex_20_fed_batch_step_new_odes.py
@@ -23,30 +23,35 @@ if __name__ == "__main__":
     
     r1.add_parameter('k1', init = 0.1)
     
-    r1.add_component('A', state = 'concentration', init = 2.0)
-    r1.add_component('B', state = 'concentration', init = 0.0)
-    r1.add_component('C', state = 'concentration', init = 0.0)
+    r1.add_component('A', state = 'concentration', init = 2.0, units='mol/L')
+    r1.add_component('B', state = 'concentration', init = 0.0, units='mol/L')
+    r1.add_component('C', state = 'concentration', init = 0.0, units='mol/L')
     
     r1.add_component('V', state = 'state', init = 1.0)
     
     filename = 'example_data/abc_fedbatch.csv'
     r1.add_dataset('C_data', category='concentration', file = filename)
     
-    r1.add_step('Qin_B', coeff=1, time=15, switch='off')
+    # Step function for B feed
+    r1.add_step('s_Qin_B', coeff=1, time=15, switch='off')
+    
+    # Volumetric flow rate for B feed
+    r1.add_constant('Qin_B', value=0.1, units='L/min')
+    
+    # Concentration of B in feed
+    r1.add_constant('Cin_B', value=2.0, units='mol/L')
     
     c = r1.get_model_vars()
-    
-    cin_B = 2
 
     # c now holds of all of the pyomo variables needed to define the equations
     # Using this object allows for a much simpler construction of expressions
     R1 = c.k1*c.A*c.B
-    Qin_B = 0.1*c.Qin_B
+    Qin_B = c.Qin_B*c.s_Qin_B
     QV = Qin_B/c.V
     
     rates = {}
     rates['A'] = -c.A*QV - R1
-    rates['B'] = (cin_B - c.B)*QV - R1
+    rates['B'] = (c.Cin_B - c.B)*QV - R1
     rates['C'] = -c.C*QV + R1
     rates['V'] = Qin_B
     
