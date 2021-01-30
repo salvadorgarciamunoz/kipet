d17f53086801dad4c99546294d31e191b7963b9f
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2021-01-30 13:49:31 -0500

    Rewrote how model components are handled

---
+++ kipet/library/model_components/ModelComponent.py
@@ -0,0 +1,188 @@
+"""
+ModelComponent Class
+"""
+
+class ModelElement():
+    
+    """Abstract Class to handle the modeling components in KIPET"""
+    
+    def __init__(self, 
+                 name=None,
+                 class_=None, 
+                 value=None,
+                 units=None
+                 ): 
+        
+        self.name = self._check_name(name)
+        self.class_ = class_
+        self.value = value
+        self.units = units
+
+    def _check_name(self, name):
+        """Check for valid attr names in the given string
+        
+        Args:
+            name (str): given name for a python attribute
+        
+        Returns:
+            checked_name (str): valid attribute name
+            
+        """
+        string_replace_dict = {
+            ' ': '_',
+            '-': 'n',
+            '+': 'p',
+            '.': '_',
+            }
+        
+        name = str(name)
+        
+        if name[0].isnumeric():
+            name = 'y' + name
+        
+        for k, v in string_replace_dict.items():
+            name = name.replace(k, v)
+        
+        return name
+    
+class ModelComponent(ModelElement):
+    """A simple class for holding component information"""
+    
+    class_ = 'model_component'
+    
+    def __init__(self,
+                 name=None,
+                 state=None,
+                 value=None,
+                 variance=None,
+                 units=None,
+                 known=True,
+                 bounds=None
+                 ):
+    
+        super().__init__(name, ModelComponent.class_, value, units)
+   
+        # component attributes
+        self.variance = variance
+        if state == 'state':
+            state = 'complementary_states'
+        self.state = state
+        self.known = known
+        self.bounds = bounds
+        
+    def __str__(self):
+        
+        margin = 25
+        settings = f'ModelComponent\n'
+        
+        for key in self.__dict__: #['name', 'class_', 'value', 'units']:
+            if key == 'class_':
+                continue
+            settings += f'{str(key).rjust(margin)} : {getattr(self, key)}\n'
+            
+        return settings
+        
+    def __repr__(self):
+        
+        return f'ModelComponent({self.name})'
+        
+        
+class ModelParameter(ModelElement):
+    """A simple class for holding kinetic parameter data
+    
+    TODO: change init to value
+    """
+
+    class_ = 'model_parameter'
+
+    def __init__(self,
+                 name,
+                 value=None,
+                 units=None,
+                 bounds=(None, None),
+                 fixed=False,
+                 variance=None,
+                 ):
+
+        super().__init__(name, ModelParameter.class_, value, units)
+        
+        # parameter attributes
+        self.bounds = bounds
+        self.fixed = fixed
+        self.variance = variance
+    
+    def __str__(self):
+        
+        margin = 25
+        settings = 'ModelParameter\n'
+        
+        for key in self.__dict__: #['name', 'class_', 'value', 'units']:
+            if key == 'class_':
+                continue
+            settings += f'{str(key).rjust(margin)} : {getattr(self, key)}\n'
+            
+        return settings
+        
+    def __repr__(self):
+        
+        return f'ModelParameter({self.name})'
+    
+    @property
+    def lb(self):
+        """Lower bound property"""
+        return self.bounds[0]
+
+    @property
+    def ub(self):
+        """Upper bound property"""
+        return self.bounds[1]
+    
+    
+class ModelConstant(ModelElement):
+    """A simple class for holding kinetic parameter data
+    
+    TODO: change init to value
+    """
+
+    class_ = 'model_constant'
+
+    def __init__(self,
+                 name,
+                 value=None,
+                 units=None,
+                 ):
+
+        super().__init__(name, ModelConstant.class_, value, units)
+        self._class_ = type(self)
+    
+    def __str__(self):
+        
+        margin = 25
+        settings = 'ModelConstant\n'
+        
+        for key in self.__dict__: #['name', 'class_', 'value', 'units']:
+            if key == 'class_':
+                continue
+            settings += f'{str(key).rjust(margin)} : {getattr(self, key)}\n'
+            
+        return settings
+        
+    def __repr__(self):
+        
+        return f'ModelConstant({self.name})'
+    
+    
+    
+ 
+# p = ModelParameter('0P', value=10, fixed=False)
+# print(p)
+
+# c = ModelComponent('A 2', value=1.0, state='concentration', variance=1e-4)
+# print(c)
+
+# k = ModelConstant('Tc', value=293.15, units='K')
+# print(k)
+    
+    
+    
+    
---
+++ kipet/library/top_level/element_blocks.py
@@ -0,0 +1,151 @@
+"""
+ModelElement Blocks
+"""
+import kipet.library.model_components.ModelComponent as model_components
+
+class ModelElementBlock():
+    
+    """Data abstraction for multiple ModelElement instances"""
+    
+    def __init__(self, class_name):
+        
+        attr_name = class_name.split('_')[-1] + 's'
+        self.attr_class_set_name = attr_name
+        setattr(self, attr_name, {})
+        
+        self.element_object_name = ''.join([term.capitalize() for term in class_name.split('_')])
+        self.element_object = getattr(model_components, self.element_object_name)
+        
+        self._dict = getattr(self, self.attr_class_set_name)
+        
+    def __getitem__(self, value):
+        
+        return getattr(self, self.attr_class_set_name)[value]
+         
+    def __str__(self):
+        
+        
+        format_string = "{:<10}{:<10}{:<15}\n"
+        param_str = f'{self.element_object_name}:\n'
+        param_str += format_string.format(*['Name', 'Value', 'Units'])
+        
+        for elem in self._dict.values():
+    
+            print(elem.name)
+            elem_name = elem.name
+            elem_value = 'None' if elem.value is None else elem.value
+            elem_units = 'None' if elem.units is None else elem.units
+
+            param_str += format_string.format(elem_name, elem_value, elem_units)
+        
+        return param_str
+
+    def __repr__(self):
+        return self.__str__()
+        # return self.element_object_name
+
+    def __iter__(self):
+        for param, data in getattr(self, self.attr_class_set_name).items():
+            yield data
+            
+    def __len__(self):
+        return len(getattr(self, self.attr_class_set_name))
+    
+
+    def add_element_list(self, elem_list):
+        """Handles lists of parameters or single parameters added to the model
+       
+        """
+        for elem in elem_list:
+            self.add_element(*elem)        
+        
+        return None
+    
+    def add_element(self, *args, **kwargs):
+        
+        """Should handle a series of different input methods:
+          
+        KP = KineticParameter('k1', init=1.0, bounds=(0.01, 10))
+        builder.add_parameter_temp(KP)
+        
+        - or -
+        
+        builder.add_parameter('k1', init=1.0, bounds=(0.01, 10))
+        
+        - or -
+        
+        builder.add_parameter('k1', 1.0, (0.01, 10))
+            
+        """
+        name = args[0]
+        element = self.element_object(name,
+                                      **kwargs,
+                                      )
+            
+        getattr(self, self.attr_class_set_name)[element.name] = element
+        
+    @property 
+    def names(self):
+        return [elem for elem in getattr(self, self.attr_class_set_name)]
+    
+    
+class ConstantBlock(ModelElementBlock):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(class_name='model_constant')
+        
+        
+class ComponentBlock(ModelElementBlock):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(class_name='model_component')
+    
+    @property
+    def variances(self):
+        return {comp.name: comp.variance for comp in self.components.values()}
+    
+    @property
+    def init_values(self):
+        return {comp.name: comp.value for comp in self.components.values()}
+    
+    @property
+    def known_values(self):
+        return {comp.name: comp.known for comp in self.components.values()}
+        
+    @property
+    def names(self):
+        return [comp.name for comp in self.components.values()]
+    
+    @property
+    def has_all_variances(self):
+    
+        all_component_variances = True
+        for comp in self.components.values():
+            if comp.variance is None:
+                all_component_variances = False
+            if not all_component_variances:
+                break
+        return all_component_variances
+    
+    
+class ParameterBlock(ModelElementBlock):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(class_name='model_parameter')
+    
+    def as_dict(self, attr):
+        
+        return_dict = {}
+        for param, obj in self.parameters.items():
+            return_dict[param] = getattr(obj, attr)
+        return return_dict
+    
+    @property
+    def lb(self):
+        """Lower bound property"""
+        return self.bounds[0]
+
+    @property
+    def ub(self):
+        """Upper bound property"""
+        return self.bounds[1]
