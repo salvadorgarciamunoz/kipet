Fixed bugs in data inputs, updated units converting

kipet/library/core_methods/PyomoSimulator.py
@@ -427,7 +427,7 @@ class PyomoSimulator(Simulator):
         # variables
         Z_var = self.model.Z
         #dZ_var = self.model.dZdt
-        P_var = self.model.P
+        #P_var = self.model.P
         #X_var = self.model.X
         #U_var = self.model.U
         #dX_var = self.model.dXdt
@@ -437,10 +437,10 @@ class PyomoSimulator(Simulator):
             Dhat_var = self.model.Dhat
             Chat_var = self.model.Chat
         # check all parameters are fixed before simulating
-        for p_var_data in P_var.values():
-            if not p_var_data.fixed:
-                raise RuntimeError(
-                    'For simulation fix all parameters. Parameter {} is unfixed'.format(p_var_data.getname()))
+        # for p_var_data in P_var.values():
+        #     if not p_var_data.fixed:
+        #         raise RuntimeError(
+        #             'For simulation fix all parameters. Parameter {} is unfixed'.format(p_var_data.getname()))
 
         # deactivates objective functions for simulation
         if self.model.nobjectives():
@@ -577,16 +577,16 @@ class PyomoSimulator(Simulator):
                         d_results.append(suma)
                         # print(d_results)
         # added due to new structure for non_abs species, non-absorbing species not included in S (CS):
-        if hasattr(self, '_abs_components'):
-            s_array = np.array(s_results).reshape((self._n_meas_lambdas, self._nabs_components))
-            results.S = pd.DataFrame(data=s_array,
-                                     columns=self._abs_components,
-                                     index=self._meas_lambdas)
-        else:
-            s_array = np.array(s_results).reshape((self._n_meas_lambdas, self._n_components))
-            results.S = pd.DataFrame(data=s_array,
-                                     columns=self._mixture_components,
-                                     index=self._meas_lambdas)
+        # if hasattr(self, '_abs_components'):
+        #     s_array = np.array(s_results).reshape((self._n_meas_lambdas, self._nabs_components))
+        #     results.S = pd.DataFrame(data=s_array,
+        #                              columns=self._abs_components,
+        #                              index=self._meas_lambdas)
+        # else:
+        #     s_array = np.array(s_results).reshape((self._n_meas_lambdas, self._n_components))
+        #     results.S = pd.DataFrame(data=s_array,
+        #                              columns=self._mixture_components,
+                                     # index=self._meas_lambdas)
 
 
         d_array = np.array(d_results).reshape((self._n_meas_times, self._n_meas_lambdas))

kipet/library/top_level/spectral_handler.py
@@ -21,12 +21,15 @@ class SpectralData():
         
         self.name = name
         self.data = data
+        self.data_orig = data
         
     def add_data(self, data):
         
         setattr(self, 'data', data)
+        if self.data_orig is None:
+            setattr(self, 'data_orig', data)
         
-    def plot(self):
+    def plot(self, data_set='data'):
         """ Plots spectral data
         
             Args:
@@ -36,11 +39,12 @@ class SpectralData():
                 None
     
         """
+        data = getattr(self, data_set)
        
         fig = go.Figure()
-        fig.add_trace(go.Surface(x=self.data.columns,
-                           y=self.data.index,
-                           z=self.data.values,
+        fig.add_trace(go.Surface(x=data.columns,
+                           y=data.index,
+                           z=data.values,
                           ))
         
         fig.update_layout(scene = dict(
@@ -55,7 +59,7 @@ class SpectralData():
             
         return None
     
-    def savitzky_golay(self, window_size, orderPoly, orderDeriv=0):
+    def savitzky_golay(self, dataFrame=None, window_size=3, orderPoly=2, orderDeriv=0, inPlace=False):
         """
         Implementation of the Savitzky-Golay filter for Kipet. Used for smoothing data, with
         the option to also differentiate the data. Can be used to remove high-frequency noise.
@@ -77,9 +81,9 @@ class SpectralData():
             Original paper: A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of Data by 
             Simplified Least Squares Procedures. Analytical Chemistry, 1964, 36 (8), pp 1627-1639.
         """
-        dataFrame = self.data
-        
         # data checks
+        if dataFrame is None:
+            dataFrame = self.data_orig
         try:
             window_size = np.abs(np.int(window_size))
             orderPoly = np.abs(np.int(orderPoly))
@@ -126,9 +130,12 @@ class SpectralData():
                                   columns = dataFrame.columns,
                                   index=dataFrame.index)
         
+        if inPlace:
+            self.data = data_frame
+        
         return data_frame
     
-    def snv(self, offset=0):
+    def snv(self, dataFrame=None, offset=0, inPlace=False):
         """
         Implementation of the Standard Normal Variate (SNV) filter for Kipet which is a weighted normalization
         method that is commonly used to remove scatter effects in spectroscopic data, this pre-processing 
@@ -148,7 +155,9 @@ class SpectralData():
         References:
     
         """
-        dataFrame = self.data
+        if dataFrame is None:
+            dataFrame = self.data_orig
+            
         # data checks
         if not isinstance(dataFrame, pd.DataFrame):
             raise TypeError("data must be inputted as a pandas DataFrame, try using read_spectral_data_from_txt or similar function first")
@@ -179,9 +188,11 @@ class SpectralData():
         data_frame = pd.DataFrame(data=snv_proc,
                                   columns = dataFrame.columns,
                                   index=dataFrame.index)
+        if inPlace:
+            self.data = data_frame
         return data_frame
     
-    def msc(self, reference_spectra=None):
+    def msc(self, dataFrame=None, reference_spectra=None, inPlace=False):
         """
         Implementation of the Multiplicative Scatter Correction (MSC) filter for Kipet which is simple pre-processing
         method that attempts to remove scaling effects and offset effects in spectroscopic data. This pre-processing 
@@ -200,7 +211,8 @@ class SpectralData():
         References:
     
         """
-        dataFrame = self.data
+        if dataFrame is None:
+            dataFrame = self.data_orig
         # data checks
         if not isinstance(dataFrame, pd.DataFrame):
             raise TypeError("data must be inputted as a pandas DataFrame, try using read_spectral_data_from_txt or similar function first")
@@ -244,9 +256,11 @@ class SpectralData():
         data_frame = pd.DataFrame(data=msc_proc,
                                   columns = dataFrame.columns,
                                   index=dataFrame.index)
+        if inPlace:
+            self.data = data_frame
         return data_frame
     
-    def baseline_shift(self, shift=None):
+    def baseline_shift(self, dataFrame=None, shift=None, inPlace=False):
         """
         Implementation of basic baseline shift. 2 modes are avaliable: 1. Automatic mode that requires no
         user arguments. The method identifies the lowest value (NOTE THAT THIS ONLY WORKS IF LOWEST VALUE
@@ -264,7 +278,8 @@ class SpectralData():
         References:
     
         """
-        dataFrame = self.data
+        if dataFrame is None:
+            dataFrame = self.data_orig
         # data checks
         if not isinstance(dataFrame, pd.DataFrame):
             raise TypeError("data must be inputted as a pandas DataFrame, try using read_spectral_data_from_txt or similar function first")
@@ -279,9 +294,12 @@ class SpectralData():
                 D[t,l] = D[t,l]+shift
         
         data_frame = pd.DataFrame(data=D, columns = dataFrame.columns, index = dataFrame.index)
+        
+        if inPlace:
+            self.data = data_frame
         return data_frame
     
-    def decrease_wavelengths(self, A_set=2, specific_subset=None):
+    def decrease_wavelengths(self, original_dataset=None, A_set=2, specific_subset=None, inPlace=False):
         '''
         Takes in the original, full dataset and removes specific wavelengths, or only keeps every
         multiple of A_set. Returns a new, smaller dataset that should be easier to solve
@@ -297,7 +315,9 @@ class SpectralData():
             DataFrame with the smaller dataset
         
         '''
-        original_dataset = self.data
+        if original_dataset is None:
+            original_dataset = self.data_orig
+            
         if specific_subset != None:
             if not isinstance(specific_subset, (list, dict)):
                 raise RuntimeError("subset must be of type list or dict!")
@@ -321,4 +341,6 @@ class SpectralData():
                 count+=1
             new_D = original_dataset[original_dataset.columns[::A_set]] 
             
+        if inPlace:
+            self.data = new_D
         return new_D