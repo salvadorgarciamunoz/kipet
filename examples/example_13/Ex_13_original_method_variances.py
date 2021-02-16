a5dcc831b4ccbc81cc043a328f2a6460838e16e2
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2021-02-16 11:47:00 -0500

    updated to new format

--- examples/example_13/Ex_13_original_method_variances.py
+++ examples/example_13/Ex_13_original_method_variances.py
@@ -21,23 +21,20 @@ if __name__ == "__main__":
     r1 = kipet_model.new_reaction('reaction-1')
 
     # Add the model parameters
-    r1.add_parameter('k1', value=1.2, bounds=(0.01, 5.0))
-    r1.add_parameter('k2', value=0.2, bounds=(0.001, 5.0))
+    k1 = r1.parameter('k1', value=1.2, bounds=(0.01, 5.0))
+    k2 = r1.parameter('k2', value=0.2, bounds=(0.001, 5.0))
     
     # Declare the components and give the initial values
-    r1.add_component('A', state='concentration', value=1e-3)
-    r1.add_component('B', state='concentration', value=0.0)
-    r1.add_component('C', state='concentration', value=0.0)
+    A = r1.component('A', state='concentration', value=1e-3)
+    B = r1.component('B', state='concentration', value=0.0)
+    C = r1.component('C', state='concentration', value=0.0)
     
     r1.add_data(category='spectral', file='data/varest2.csv', remove_negatives=True)
 
-    # define explicit system of ODEs
-    c = r1.get_model_vars()
-    # define explicit system of ODEs
     rates = {}
-    rates['A'] = -c.k1 * c.A
-    rates['B'] = c.k1 * c.A - c.k2 * c.B
-    rates['C'] = c.k2 * c.B
+    rates['A'] = -k1 * A
+    rates['B'] = k1 * A - k2 * B
+    rates['C'] = k2 * B
     
     r1.add_odes(rates)
 
