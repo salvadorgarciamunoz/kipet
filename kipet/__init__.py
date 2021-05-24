bc8fa48a07baa4851db05d14a5703752a7cabb15
kwmcbride <kevin.w.mcbride.86@gmail.com>
2021-05-24 18:28:04 +0200

    Added ReactionModel, ReacitonLab, and IO

--- kipet/__init__.py
+++ kipet/__init__.py
@@ -4,5 +4,7 @@ from .top_level.settings import Settings
 
 kipet.settings = Settings()
 
-from .core_methods.data_tools import *
-from .kipet_model import KipetModel
+#from .core_methods.data_tools import *
+from .kipet_model import ReactionLab
+from .top_level.reaction_model import ReactionModel
+from .kipet_io import *
