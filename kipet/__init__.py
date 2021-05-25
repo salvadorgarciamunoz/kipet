857eea67839711144da8e04eea69db4322d150d3
kwmcbride <kevin.w.mcbride.86@gmail.com>
2021-05-25 20:29:53 +0200

    changed Lab to Set

--- kipet/__init__.py
+++ kipet/__init__.py
@@ -5,6 +5,6 @@ from .top_level.settings import Settings
 kipet.settings = Settings()
 
 #from .core_methods.data_tools import *
-from .kipet_model import ReactionLab
+from .kipet_model import ReactionSet
 from .top_level.reaction_model import ReactionModel
 from .kipet_io import *
