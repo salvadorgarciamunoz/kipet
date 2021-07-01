9c06d5951a3b040355063936c9f6a6650c2ae65e
kwmcbride <kevin.w.mcbride.86@gmail.com>
2021-07-01 11:31:27 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='kipet',
-    version='0.2.3',
+    version='1.0.1',
     description='An all-in-one tool for fitting kinetic models using spectral and other state data',
     python_requires='==3.*,>=3.8.0',
     project_urls={
@@ -31,9 +31,9 @@ setup(
     license='GPL-3.0-or-later',
     keywords='optimization scientific parameter reaction spectral',
     packages=[
-        'kipet', 'kipet.calculation_tools', 'kipet.core_methods', 'kipet.dev_tools',
-        'kipet.mixins', 'kipet.model_components',
-        'kipet.model_tools', 'kipet.general_settings', 'kipet.variance_methods',
+        'kipet', 'kipet.calculation_tools', 'kipet.estimability_tools', 'kipet.estimator_tools',
+        'kipet.general_settings', 'kipet.input_output', 'kipet.main_modules', 'kipet.mixins',
+	'kipet.model_components', 'kipet.model_tools', 'kipet.variance_methods',
         'kipet.visuals'
     ],
     package_dir={"": "."},
@@ -44,9 +44,8 @@ setup(
         'attrs==20.*,>=20.3.0', 'matplotlib==3.*,>=3.3.4',
         'numpy==1.*,>=1.20.1', 'pandas==1.*,>=1.2.2', 'pint==0.*,>=0.16.1',
         'plotly==4.*,>=4.14.3', 'pyomo==5.*,>=5.7.3', 'pyyaml==5.*,>=5.4.1',
-        'scipy==1.*,>=1.6.0'
+        'scipy==1.*,>=1.6.0', 'kaleido==0.2.1', 'jinja2==3.0.1', 'pytexit==0.3.4',
     ],
-    extras_require={"dev": ["pytest==5.*,>=5.2.0", "spyder==4.*,>=4.2.2",
-                            "isort==5.*,>=5.8.0", "mypy>=0.812", "sphinx-rtd-theme==0.5.2"]},
+    extras_require={"dev": ["pytest==5.*,>=5.2.0", "isort==5.*,>=5.8.0", "mypy>=0.812", "sphinx-rtd-theme==0.5.2"]},
 
 )
